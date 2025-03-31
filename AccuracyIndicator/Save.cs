9d7e3a6728e17e57f4488a0633a9bc8b3e6987e8
Vaquh <Vaquh@users.noreply.github.com>
2025-03-31 08:46:23 -0400

    Change config file to AccuracyIndicator.cfg

--- AccuracyIndicator/Save.cs
+++ AccuracyIndicator/Save.cs
@@ -3,15 +3,16 @@ namespace AccuracyIndicator;
 
 internal static class Save
 {
-    private static MelonPreferences_Category prefsCategory; 
+    private static MelonPreferences_Category accuracyIndicatorCategory; 
     private static MelonPreferences_Entry<bool> showMeanDelay;
 
     internal static bool ShowMeanDelay => showMeanDelay.Value;
 
     public static void Load()
     {
-        prefsCategory = MelonPreferences.CreateCategory(MelonBuildInfo.Name);
-        showMeanDelay = prefsCategory.CreateEntry<bool>("ShowMeanDelay", true,
+        accuracyIndicatorCategory = MelonPreferences.CreateCategory(MelonBuildInfo.Name);
+        accuracyIndicatorCategory.SetFilePath("UserData/AccuracyIndicator.cfg");
+        showMeanDelay = accuracyIndicatorCategory.CreateEntry<bool>("ShowMeanDelay", true,
         "Victory screen mean delay", "Enables Mean Delay display in victory screen.");
     }
 }
